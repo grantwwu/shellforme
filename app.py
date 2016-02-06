000de9983a684321a15042d650f2fb2d41eddee8
Grant Wu <grantwwu@gmail.com>
2016-02-06 12:50:10 -0500

    Send favicon.ico

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/favicon.png')
+def index():
+    return app.send_static_file('favicon.png')
+
 @app.route('/compile', methods=['POST'])
 def compile():
     ir = request.get_json()
-    try
+    try:
         ret = compile(json.loads(p))
     except compiler.Error as exception:
         ret = exception.as_dict()
