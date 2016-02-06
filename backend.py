dbb238cd0465e6da65caa6a418825762709a5127
Robin Tan <robintan@andrew.cmu.edu>
2016-02-06 08:51:20 -0500

    bash works for basic inputs

--- backend.py
+++ backend.py
@@ -18,21 +18,23 @@
  # "\e[38;5;${i}m#\e[0m"
 
 def bash_script(L):
+  #color:color code
   bashColors = {"black":("0","30"), "gray":("1","30"),
               "blue":("0","34"), "green":("0","32"),
               "cyan":("0","36"), "red":("0","31"),
               "purple":("0","35"), "brown":("0","33"),
               "yellow":("1","33"), "white":("1","37")}
+  #json name:escape key  basic arguments
   bashCmd = {"username":"\\u ", "hostname":"\\h ",
              "fqdn":"\\H ", "path":"\\w ", "pwd":"\\W ",
              "date":"\\d ", "24h":"\\A ", "12h":"\\@ ", 
              "24hs":"\\t ", "newline":"\n ", "history":"\\! ",
-             "@":"@ ", ":":": ", ">":"> ", "~":"~ "}
+             "@":"@ ", ":":": ", ">":"> "}
   src = ""
   for op in L:
     op_type = op["type"]
     if not op_type in bashCmd:
-        raise Exception("This is not a valid command yet")
+      raise Exception("This is not a valid command yet")
 
     cmd = bashCmd[op_type]
 
@@ -40,7 +42,6 @@ def bash_script(L):
       c1, c2 = bashColors[op["color"]]
       cmd = "\\033[{0};{1}m{2}\\e[0m ".format(c1, c2, cmd)
 
-    print("cmd", cmd)
     src += cmd
 
   return src
